4f05d52f19e04b1da60df1c596914e7ff343ce5f
ndayiringiye david <ndayiringiyedavid120@gmail.com>
2025-02-08 16:17:24 +0200

    project setup successfully

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const App = () => {
+  const par= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, dolores laborum quod ipsum autem atque unde odio quo illo aperiam sapiente, voluptatum excepturi tempore beatae officiis rem earum cumque blanditiis?";
+  const [isLargeWord, setIsLargeWord] = useState(false)
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
